5bc9e3925aad044b4abe65ea266807f8b4c6d1ff
Alexei Torchinsky <alextor1005@gmail.com>
2023-02-27 02:04:28 +0300

    feat: compl task

--- src/index.js
+++ src/index.js
@@ -37,9 +37,45 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+
 function decode(expr) {
-    // write your solution here
-}
+    const arr = expr.match(/(.{10}|.)/g);
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i] === '**********') {
+            arr[i] = ' '
+        }
+    }
+    
+    for (let i = 0; i < arr.length; i++){
+        arr[i] = arr[i].match(/(.{2}|.)/g);
+        
+    }
+    for (let i = 0; i < arr.length; i++){
+        for (let j = 0; j < arr[i].length; j++) {
+            if(arr[i][j] === '00') {
+                arr[i][j] = '';
+            };
+            if(arr[i][j] === '10') {
+                arr[i][j] = '.' 
+            }
+            if(arr[i][j] === '11') {
+                arr[i][j] = '-'
+            }
+        }
+    }
+    for (let i = 0; i < arr.length; i++){
+        arr[i] = arr[i].join('')
+    }
+    for(let i = 0; i < arr.length; i++) {
+        for(let key in MORSE_TABLE){
+            if (key === arr[i]) {
+                arr[i] = MORSE_TABLE[key]
+            }
+        }
+    }
+    const res = arr.join('')  
+    return res; 
+    }    
 
 module.exports = {
     decode
